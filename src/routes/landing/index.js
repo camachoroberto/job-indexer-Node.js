7b0ff2998a8d21d1da2a928f052a19060c4577b0
Roberto Camacho <camachof.roberto@gmail.com>
2019-09-10 16:43:31 -0300

    feat(login): login template creation
    developing layouts and routes
    
    task 2

--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import landing from "./../../services/landing/";
+import login from "./../../services/login/";
 
 const router = express.Router();
 
 router.get("/", landing);
+router.get("/login", login);
 
 module.exports = router;
